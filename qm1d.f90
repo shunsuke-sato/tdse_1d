09acfdc5294324276dfd8236bcfc2d9f6541f1bf
shunsuke <shunsuke.sato26@gmail.com>
2016-06-18 15:58:31 +0200

    Add license copy.

--- qm1d.f90
+++ qm1d.f90
@@ -1,3 +1,8 @@
+!******************************************************
+! Copyright (c) 2016 Shunsuke A. Sato                 *
+! Released under the MIT license                      *
+! http://opensource.org/licenses/mit-license.php      *
+!******************************************************
 module gloval_variables
 
 ! parameter
